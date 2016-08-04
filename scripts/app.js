c153b8a6edc17884e78238ce54224150f03b7f93
Natalia Polishchuk <Natalia@natalias-mbp.gateway.sonic.net>
2016-08-03 18:49:56 -0700

    add  search feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,25 @@
-angular.module('your-app-name-goes-here', []);
-       //.config(config);
-
+angular.module('MusicExplorer',['ngRoute'])
+       .config(config);
 
 ////////////
 // ROUTES //    //  SAVE THIS FOR LATER
 ////////////
 
-// config.$inject = ['$routeProvider', '$locationProvider'];
-// function config (  $routeProvider,   $locationProvider  )  {
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: '',
-//       controller: '',
-//       controllerAs: ''
-//     })
-//     .otherwise({
-//       redirectTo: '/'
-//     });
-//
-//   $locationProvider
-//     .html5Mode({
-//       enabled: true,
-//       requireBase: false
-//     });
-// }
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/example-index.template.html',
+      controller: 'MusicController',
+      controllerAs: 'musicCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
--- scripts/controllers/example.controller.js
+++ scripts/controllers/example.controller.js
@@ -1,17 +1,28 @@
-angular.module('your-app-name-goes-here')
-  .controller('ExampleController', ExampleController);
+angular.module('MusicExplorer')
+  .controller('MusicController', MusicController);
 
-ExampleController.$inject=[];
-function ExampleController() {
-  console.log("ExampleController!");
+MusicController.$inject=['$http'];
+function MusicController($http) {
+  console.log("MusicController!");
 
   var vm = this;
 
-  vm.sayHi = sayHi;
 
   ////
+vm.find = function() {
+  console.log(vm.search);
+  var myUrl = 'https://api.spotify.com/v1/search?q=' +vm.search+ "&type=artist";
+  console.log(myUrl);
+  $http({
+    method: 'GET',
+    url: myUrl
+  }).then(function successCallback(response) {
+    console.log(response.data);
+    vm.artists = response.data.artists.items;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+};
 
-  function sayHi(name) {
-    return "Hello " + name;
-  }
+
 }
